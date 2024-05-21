b0ebc9174d2b745cb0fa99149a71822c49959aaa
Wahab Syed <wahasyed@amazon.com>
2024-05-21 16:19:30 -0700

    feat: adding aurora mysql source db

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ import os
 import aws_cdk as cdk
 
 from networking.component import Networking
+from backend.component import Backend
 from data_streaming.component import DataStreaming
 
 app = cdk.App()
 
+# Create the networking stack
 networking = Networking(app, "Networking")
+
+# Create the backend stack
+backend = Backend(app, "Backend", vpc=networking.vpc)
+backend.add_dependency(networking)
+
+# Create the data streaming stack
 data_straming = DataStreaming(app, "DataStreaming", vpc=networking.vpc)
 data_straming.add_dependency(networking)
 
--- backend/component.py
+++ backend/component.py
@@ -1,20 +1,47 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_rds as rds,
 )
 from constructs import Construct
 
 
 class Backend(Stack):
+    """
+    Aurora MySQL cluster with 2 read replicas in a VPC
+    """
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+        self, scope: Construct, construct_id: str, vpc: ec2.IVpc, **kwargs
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsTemplateQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create an RDS Aurora MySQl cluster with 2 read replica in a vpc
+        rds.DatabaseCluster(
+            self,
+            "source-mysql",
+            engine=rds.DatabaseClusterEngine.aurora_mysql(
+                version=rds.AuroraMysqlEngineVersion.VER_3_06_0
+            ),
+            credentials=rds.Credentials.from_generated_secret("clusteradmin"),
+            writer=rds.ClusterInstance.provisioned(
+                "writer",
+                instance_type=ec2.InstanceType.of(
+                    ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.MEDIUM
+                ),
+            ),
+            readers=[
+                rds.ClusterInstance.provisioned(
+                    "reader",
+                    instance_type=ec2.InstanceType.of(
+                        ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.MEDIUM
+                    ),
+                )
+            ],
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
+            ),
+            backtrack_window=Duration.seconds(30),
+        )
